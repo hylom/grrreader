093fdc718dc752aad63fbba0ce9c4ba704410b05
hylom <hylom@hylom.net>
2013-06-22 23:08:37 +0900

    update testcase

--- t/mysqldb.py
+++ t/mysqldb.py
@@ -7,7 +7,7 @@ class TestMySQLDatabase(unittest.TestCase):
     "test case for MySQLDatabase"
     def setUp(self):
         self.mysql = MySQLDatabase()
-        #self.mysql.config["database"] = ""
+        self.mysql.config["database"] = "testdb"
         self.mysql.create_tables()
 
     def test_add_feed(self):
