1f157356bfed231585ca0255bfe5640cd654d3d6
hylom <hylom@hylom.net>
2013-07-04 01:16:30 +0900

    implement contents preview

--- client/public/js/grdc.js
+++ client/public/js/grdc.js
@@ -1,18 +1,23 @@
 // grdc.js
 
 (function () {
-
   function updateContentsPane(data) {
     var pane = $('#contentsPane');
     pane.empty();
     for (var i = 0; i < data.length; i++) {
       var item = data[i];
-      var elem = '<tr>'
-               + '<td>' + item.title + '</td>'
-               + '<td>' + item.formatedTimestamp + '</td>'
-               + '<td><a target="_blank" href="' + item.url + '">'
-               + '<i class="icon-chevron-right"></i></a></td>'
-               + '</tr>';
+      var elem = '<tr class="contentHeader" id="chead' + item.content_id + '">'
+        + '<td class="contentTitle"><h4><a href="#" class="contentTitleString" cid="' + item.content_id + '">' + item.title + '</a></h4></td>'
+        + '<td class="contentTimestamp">' + item.formatedTimestamp + '</td>'
+        + '<td class="contentLink"><a target="_blank" href="' + item.url + '">'
+        + '<i class="icon-chevron-right"></i></a></td>'
+        + '</tr>'
+        + '<tr class="contentBody" id="cbody' + item.content_id + '">'
+        + '<td colspan="3">'
+        + item.body
+        + '</td>'
+        + '</tr>'
+      ;
       pane.append(elem);
     }
   }
@@ -26,7 +31,17 @@
     return false;
   }
 
+  function toggleContentBody(event) {
+    var contentId = $(event.target).attr('cid');
+    console.log('click header ' + contentId);
+    if (contentId !== undefined) {
+      $('#cbody' + contentId).toggle();
+    }
+    return false;
+  }
+
   $(document).on('click', ".feedItem", loadFeed);
+  $(document).on('click', ".contentTitleString", toggleContentBody);
 }).apply();
 
 
