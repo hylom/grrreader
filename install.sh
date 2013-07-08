afad4a4da77caed3830475d35d5bfec6a024ae82
hylom <hylom@hylom.net>
2013-07-08 19:41:25 +0900

    fix some scripts

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ DEST=/var/grdc/
 INITSCRIPT=./initscript
 
 rsync $1 -av --exclude=.* --exclude=*~ $FROM $DEST
-install -pm 755 $INITSCRIPT /etc/init.d/rssreader
+install -pm 755 $INITSCRIPT /etc/init.d/rrreader
