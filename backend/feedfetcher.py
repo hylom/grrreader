ea2232ba2b1a8221604017c9a06f33f18d4058fb
hylom <hylom@hylom.net>
2015-06-17 17:10:06 +0900

    fix crash when invalid url inputed

--- backend/feedfetcher.py
+++ backend/feedfetcher.py
@@ -19,7 +19,10 @@ def main():
             entries = fetcher.fetch(url)
             for entry in entries:
                 entry.feed_id = fid
-                print "insert {0}".format(entry.url)
+                try:
+                    print "insert {0}".format(entry.url)
+                except UnicodeEncodeError:
+                    print "insert {0}".format(entry.url.encode('utf-8'))
                 db.append_feed_content(entry)
         feeds = db.get_feeds(offset=read_count, limit=10)
         read_count += 10
